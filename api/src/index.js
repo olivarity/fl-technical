19453b96ca0cee9e73472ce333a38f541c493f75
Olivia Isaacs <olivia@scaasic.com>
2024-09-20 08:56:39 -0500

    add bodyparser

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,10 @@
 import Koa from "koa";
+import { bodyParser } from "@koa/bodyparser";
 import { studentsRouter } from "./students/routes.js";
 
 const app = new Koa();
+
+app.use(bodyParser());
 
 app.use(studentsRouter.routes());
 app.use(studentsRouter.allowedMethods());
